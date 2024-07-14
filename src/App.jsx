4a2495787714e5a971a3a0adc817575251c87ec9
Tyler <tybeqqfn@gmail.com>
2024-07-14 15:15:20 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import jiujitsuLogo from './assets/Jiujitsu.svg'
+import {words}  from './wordlist.js'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [randomWord, setRandomWord] = useState('');
+
+  const generateWord = () => {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    setRandomWord(words[randomIndex])
+  }
+
+  return (
+    <>
+      <h1>BJJ Technique Generator</h1>
+      <h3>A guide to your Jiu Jitsu Journey.</h3>
+      <div className="card">
+        <button onClick={generateWord}>Generate Technique</button>
+        <p>You should train {randomWord} this week.</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/wordlist.js
@@ -0,0 +1,8 @@
+export const words = [
+    'De La Riva', 'Reverse De La Riva', 'Closed Guard', 'Half Guard', 'Mount',
+    'Butterfly Guard', 'Back Mount', 'Armbars', 'Leg Locks', 'Passing Guard',
+    'Spider Guard', 'Open Guard', 'Side Control', 'North South', 'Knee on Belly',
+    '50/50', 'X Guard', 'Deep Half Guard', 'Rubber Guard', 'Omoplata', 'Triangles',
+    'Kimuras', 'Americanas', 'Guillotines', 'Collar Chokes', 'Back Side 50/50', 'Worm Guard',
+    'Lapel Guard', 'Berimbolos',
+  ];
